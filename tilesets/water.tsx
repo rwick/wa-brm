44efe2e077893cb548dde6c8fff20ec2960745c7
Remi Winckel <rwinckel@scaleway.com>
2023-02-01 08:59:29 +0100

    fix embedded tilset

--- tilesets/water.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="water" tilewidth="32" tileheight="32" tilecount="768" columns="64">
- <image source="water.png" width="2048" height="384"/>
-</tileset>
